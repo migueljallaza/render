36042625a61e0fe610510aee31346ebbbcce1a65
franzmjc <franzmjc@gmail.com>
2023-01-17 00:03:23 -0400

    quitado el cors1

--- src/main/java/com/example/demo/controller/Usuario.java
+++ src/main/java/com/example/demo/controller/Usuario.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 
 @RestController
+@CrossOrigin(origins = "https://mike2023.netlify.app")
 @RequestMapping("/user/")
 public class Usuario {
 
