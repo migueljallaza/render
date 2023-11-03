3d3860a843b1641420c17f503e51f702f9dbbfc8
migueljallaza <38589552+migueljallaza@users.noreply.github.com>
2023-11-02 20:29:08 -0400

    Update AreaController.java

--- src/main/java/com/example/demo/controller/AreaController.java
+++ src/main/java/com/example/demo/controller/AreaController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "*", methods= {RequestMethod.POST,RequestMethod.GET})    
 @RequestMapping("/area/")
 
 public class AreaController {
